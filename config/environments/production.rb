cf12190cf9a2e065bf052eef218e56bc3a4339c4
clement-chappron <clement.chappron@gmail.com>
2024-08-10 15:21:36 +0400

    configuration de l'app et mise en place de device

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'airbnb-clement-chappron-e72245852398.herokuapp', port: 3000 }
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
