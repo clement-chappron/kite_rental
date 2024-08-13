f299a05368bbe2db9f41efe37faf258afc364dbe
Eldave DIBALA <eldavedibala@gmail.com>
2024-08-13 17:40:51 +0100

    Done with the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  has_many :products
-  has_many :bookings
-
-  # affiche tous les produits qu'un utilisateur a loué
-  # has_many :products, through: :bookings
-
+  has_many :products, foreign_key: :'id_users' # un user peut avoir des products
+  has_many :bookings, foreign_key: :'id_users' # un user peut avoir des réza
+  has_many :reviews, foreign_key: :'id_users' # Un user peut laisser des reviews
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
