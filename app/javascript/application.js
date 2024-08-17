da2cb8a1c25e6ed6235e4ebc54f483c533e8e30b
Avinash Daby <djavinash@yahoo.co.uk>
2024-08-17 17:53:14 +0400

    Tried to fix dates with flatpickr

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,5 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+import "flatpickr"
+document.addEventListener('turbo:load', () => { flatpickr(".datepicker", {}); });
