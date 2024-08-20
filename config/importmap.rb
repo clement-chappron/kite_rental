e434696194bd17d26654500316202a5b00344642
clement-chappron <clement.chappron@gmail.com>
2024-08-20 12:06:50 +0200

    creation du banner de la home page

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "flatpickr" # @4.6.13
+pin "typed.js" # @2.1.0
