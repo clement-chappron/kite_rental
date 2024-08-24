a4aeb3abf901dd74024a7aecdd4f8c782de3be18
Eldave DIBALA <eldavedibala@gmail.com>
2024-08-24 09:26:05 +0100

    Done with mon profil updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
   def profile
     @user = current_user
   end
+
+  def update_profile_picture
+    if current_user.update(profile_picture: params[:user][:profile_picture])
+      render json: { profile_picture_url: current_user.profile_picture.url }, status: :ok
+    else
+      render json: { error: 'Erreur lors de la mise à jour de la photo' }, status: :unprocessable_entity
+    end
+  end
 end
